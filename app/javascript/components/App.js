2ca9458c6912954d81f2020aadc7d752a02e46a1
Neeraj Keriya <neerajkeriya@gmail.com>
2022-05-17 23:56:32 +0530

    Added bootsrap(Navbar) and axios

--- app/javascript/components/App.js
+++ app/javascript/components/App.js
@@ -2,11 +2,14 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Airline from "./Airline/Airline";
 import Airlines from "./Airlines/Airlines";
+import 'bootstrap/dist/css/bootstrap.min.css'
+import Navbar from "./Navbar";
 
 export default function App() {
   return (
     <>
       <BrowserRouter>
+      <Navbar/>
         <Routes>
           <Route exact path="/" element={<Airlines/>}/>
           <Route
